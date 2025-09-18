5f5ab6cb09fc5462f5574533611ae74386e3b81d
maucon <maucon13@gmail.com>
2025-09-18 12:54:57 +0200

    add event publish method

--- src/main/kotlin/de/maucon/mauconframework/event/EventGateway.kt
+++ src/main/kotlin/de/maucon/mauconframework/event/EventGateway.kt
@@ -1,5 +1,6 @@
 package de.maucon.mauconframework.event
 
+import de.maucon.mauconframework.event.EventGateway.eventScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -8,7 +9,7 @@ import kotlinx.coroutines.launch
 /**
  * A gateway for publishing events to the event bus.
  *
- * This object provides a method to publish events asynchronously using the `EventBus`.
+ * This object provides a method to publish events asynchronously using the [EventBus].
  */
 object EventGateway {
     /**
@@ -37,6 +38,21 @@ object EventGateway {
      */
     fun <T : Any> launchPublish(event: T) {
         eventScope.launch {
+            EventBus.publish(event)
+        }
+    }
+
+    /**
+     * Publishes the given event to the event bus asynchronously using the provided [scope].
+     *
+     * This method is non-blocking and intended for use in non-suspending contexts.
+     * It launches a coroutine within the given [scope] to publish the event.
+     *
+     * @param scope The [CoroutineScope] in which the coroutine will be launched.
+     * @param event The event instance to be published.
+     */
+    fun <T : Any> launchPublish(scope: CoroutineScope, event: T) {
+        scope.launch {
             EventBus.publish(event)
         }
     }
---
+++ src/test/kotlin/de/maucon/mauconframework/event/test010/Test010.kt
@@ -0,0 +1,55 @@
+package de.maucon.mauconframework.event.test010
+
+import de.maucon.mauconframework.MauConFramework
+import de.maucon.mauconframework.di.annotation.Injectable
+import de.maucon.mauconframework.event.EventGateway
+import de.maucon.mauconframework.event.EventSubscriber
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.slf4j.LoggerFactory
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+private val log = LoggerFactory.getLogger(Test010::class.java)
+
+@DisplayName("External event scope")
+object Test010 {
+    private val externalScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
+    var callCounter = AtomicInteger(0)
+
+    @Test
+    fun test001() = runBlocking {
+        val job = CoroutineScope(Dispatchers.Default).launch {
+            val components = assertDoesNotThrow { MauConFramework.start(Test010::class.java) }
+            val componentClasses = components.map { it.javaClass }
+            assertContains<Class<*>>(componentClasses, EventSub1::class.java)
+
+        }
+        job.join()
+
+        for (i in 0..1) {
+            delay(10)
+            EventGateway.launchPublish(externalScope, TestEvent("launchPublish externalScope Test001 $i"))
+            delay(10)
+            EventGateway.launchPublish(TestEvent("launchPublish Test001 $i"))
+        }
+        delay(10)
+
+        assertEquals(4, callCounter.get())
+    }
+}
+
+@Injectable
+class EventSub1 {
+    @EventSubscriber
+    fun on(event: TestEvent) {
+        log.info("EventSub1: got event: $event")
+        Test010.callCounter.incrementAndGet()
+    }
+}
+
+data class TestEvent(val name: String)
