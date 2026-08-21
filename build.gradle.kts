a9f9b37aaa5f9d75d080e0946fa275c2eba927b5
maucon <maucon13@gmail.com>
2026-08-21 10:54:19 +0200

    bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
+    kotlin("jvm") version "2.4.0"
     `maven-publish`
 }
 
@@ -11,13 +11,13 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("reflect", "2.3.20"))
+    implementation(kotlin("reflect", "2.4.0"))
     implementation("org.reflections:reflections:0.10.2")
-    implementation("org.slf4j:slf4j-api:2.0.17")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.slf4j:slf4j-api:2.0.18")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
 
-    testImplementation(kotlin("test", "2.3.10"))
-    testImplementation("ch.qos.logback:logback-classic:1.5.32")
+    testImplementation(kotlin("test", "2.4.0"))
+    testImplementation("ch.qos.logback:logback-classic:1.6.3")
 }
 
 tasks.test {
